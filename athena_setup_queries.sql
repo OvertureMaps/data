8e77e4cbb3b3b9dbab26cb51e9fb6ec1fddc7801
Jennings Anderson <jennings.anderson@gmail.com>
2023-07-25 14:30:43 -0700

    adding the create table queries

--- athena_setup_queries.sql
+++ athena_setup_queries.sql
@@ -1,34 +1,72 @@
--- Setting up the Overture Maps Data Tables on AWS Athena
+-- Setting up the Overture Maps Data Tables on Amazon Athena
 
 -- Admins theme
+CREATE EXTERNAL TABLE `admins`(
+  `id` string,
+  `updatetime` string,
+  `version` int,
+  `names` map<string,array<map<string,string>>>,
+  `adminlevel` int,
+  `maritime` string,
+  `subtype` string,
+  `localitytype` string,
+  `context` string,
+  `isocountrycodealpha2` string,
+  `isosubcountrycode` string,
+  `defaultlanugage` string,
+  `drivingside` string,
+  `sources` array<map<string,string>>,
+  `bbox` struct<minx:double,maxx:double,miny:double,maxy:double>,
+  `geometry` binary)
+PARTITIONED BY (
+  `type` string)
+STORED AS PARQUET
+LOCATION
+  's3://overturemaps-us-west-2/release/2023-07-26.0/theme=admins'
+
 
 -- Buildings theme
 CREATE EXTERNAL TABLE `buildings`(
-  `theme` varchar(9),
-  `version` int,
+  `id` string,
   `updatetime` string,
+  `version` int,
+  `names` map<string,array<map<string,string>>>,
+  `level` int,
   `height` double,
   `numfloors` int,
-  `level` int,
   `class` string,
-  `names` map<string,array<map<string,string>>>,
   `sources` array<map<string,string>>,
-  `geometry` string,
-  `bbox` struct<minx:double,maxx:double,miny:double,maxy:double>)
+  `bbox` struct<minx:double,maxx:double,miny:double,maxy:double>,
+  `geometry` binary)
 PARTITIONED BY (
   `type` varchar(8))
 STORED AS PARQUET
 LOCATION
-  's3://omf-internal-usw2/staging/buildings'
-TBLPROPERTIES (
-  'auto.purge'='false',
-  'compression.level'='3',
-  'has_encrypted_data'='false',
-  'parquet.compression'='zstd'
-)
+  's3://overturemaps-us-west-2/release/2023-07-26.0/theme=buildings'
+
 
 -- Places theme
-
+CREATE EXTERNAL TABLE `places`(
+  `id` string,
+  `updatetime` string,
+  `version` int,
+  `names` map<string,array<map<string,string>>>,
+  `categories` struct<main:string,alternate:array<string>>,
+  `confidence` double,
+  `websites` array<string>,
+  `socials` array<string>,
+  `emails` array<string>,
+  `phones` array<string>,
+  `brand` struct<names:map<string,array<map<string,string>>>,wikidata:string>,
+  `addresses` array<map<string,string>>,
+  `sources` array<map<string,string>>,
+  `bbox` struct<minx:double,maxx:double,miny:double,maxy:double>,
+  `geometry` binary)
+PARTITIONED BY (
+  `type` varchar(5))
+STORED AS PARQUET
+LOCATION
+  's3://overturemaps-us-west-2/release/2023-07-26.0/theme=places'
 
 
 -- Transportation theme
