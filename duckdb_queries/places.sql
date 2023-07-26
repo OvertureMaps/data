479d0187c9e9fd71b3354e18e3fed89307bbfad0
Jennings Anderson <jennings.anderson@gmail.com>
2023-07-26 16:40:52 -0400

    places

--- duckdb_queries/places.sql
+++ duckdb_queries/places.sql
@@ -1,10 +1,11 @@
 LOAD httpfs;
 LOAD spatial;
 
-COPY (
+-- This will create a 5mb GeoJSONSeq file with 23k places in Seattle.
 
+COPY (
     SELECT
-       CAST(names AS JSON),
+       CAST(names AS JSON) as name,
        ST_GeomFromWKB(geometry)
     FROM
        read_parquet('s3://overturemaps-us-west-2/release/2023-07-26-alpha.0/theme=places/type=*/*', hive_partitioning=1)
@@ -15,3 +16,10 @@ COPY (
         AND bbox.maxy < 47.7120663
     ) TO 'places_seattle.geojsonseq'
 WITH (FORMAT GDAL, DRIVER 'GeoJSONSeq');
+
+-- Tip: Replace the last 2 lines with:
+--
+--      ) TO 'places_seattle.shp'
+--   WITH (FORMAT GDAL, DRIVER 'ESRI Shapefile');
+--
+-- to write the data directly to a shapefile.
