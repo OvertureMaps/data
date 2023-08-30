2848ff78c95f0256918e8e1ae71c1fd9196cd991
Jennings Anderson <jennings.anderson@gmail.com>
2023-08-30 07:58:22 -0700

    upating last minute

--- duckdb_queries/places.sql
+++ duckdb_queries/places.sql
@@ -5,7 +5,19 @@ LOAD spatial;
 
 COPY (
     SELECT
-       CAST(names AS JSON) as name,
+       id,
+       updatetime,
+       version,
+       CAST(names AS JSON) AS names,
+       CAST(categories AS JSON) AS categories,
+       confidence,
+       CAST(websites AS JSON) AS websites,
+       CAST(socials AS JSON) AS socials,
+       CAST(emails AS JSON) AS emails,
+       CAST(phones AS JSON) AS phones,
+       CAST(brand AS JSON) AS brand,
+       CAST(addresses AS JSON) AS addresses,
+       CAST(sources AS JSON) AS sources,
        ST_GeomFromWKB(geometry)
     FROM
        read_parquet('s3://overturemaps-us-west-2/release/2023-07-26-alpha.0/theme=places/type=*/*', hive_partitioning=1)
