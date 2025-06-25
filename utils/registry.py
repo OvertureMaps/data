e6ddd63dd96c1b34b5f6b82bb42e3ecb15afe6ea
Jennings Anderson <jenningsa@meta.com>
2025-06-25 14:04:19 -0300

    Update registry.py

--- utils/registry.py
+++ utils/registry.py
@@ -4,8 +4,8 @@ import json
 
 import duckdb
 
-REGISTRY_PATH = "s3://overturemaps-extras-us-west-2/maproulette/registry-test/*.parquet"
-RELEASE_PATH = "s3://overturemaps-us-west-2/release/2025-03-19.1/"
+REGISTRY_PATH = "s3://overturemaps-us-west-2/registry/*.parquet"
+RELEASE_PATH = "s3://overturemaps-us-west-2/release/2025-06-25.0/"
 
 
 class RegistryQuery:
